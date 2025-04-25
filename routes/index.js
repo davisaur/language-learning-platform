3e2d4b10197e3f45c5b286dd18a69602bcd20e84
davi <39279817+davisaur@users.noreply.github.com>
2025-04-26 00:45:37 +0100

    redirect user to login if not logged in

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+// Check if the user session exists
+const checkUserSession = (req, res, next) => {
+    if (!req.session.user) {
+        return res.redirect('/auth/login');
+    }
+    next();
+};
+
 // GET / route
-router.get('/', (req, res) => {
+router.get('/', checkUserSession, (req, res) => {
     res.render('create');
 });
 
