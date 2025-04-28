57e6852643cb0f526d55f4c7e8132328a2764b88
davi <39279817+davisaur@users.noreply.github.com>
2025-04-28 21:42:58 +0100

    working navbar with course

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,39 @@
 const express = require('express');
 const router = express.Router();
 
+const languages = [{
+    "language": "Spanish",
+    "lessons": {
+        "A1": [
+            { 
+                "id": "1",
+                "title": "Greetings and Introductions", 
+                "description": "Learning basic greetings and farewells." 
+            },
+            { 
+                "id": "2",
+                "title": "Talking about yourself", 
+                "description": "Describing yourself and where you're from." 
+            },
+            { 
+                "id": "3",
+                "title": "Talking about yourself", 
+                "description": "Describing yourself and where you're from." 
+            },
+            { 
+                "id": "4",
+                "title": "Talking about yourself", 
+                "description": "Describing yourself and where you're from." 
+            },
+            { 
+                "id": "5",
+                "title": "Talking about yourself", 
+                "description": "Describing yourself and where you're from." 
+            }
+        ]
+    }
+}];
+
 // Check if the user session exists
 const checkUserSession = (req, res, next) => {
     if (!req.session.user) {
@@ -11,7 +44,17 @@ const checkUserSession = (req, res, next) => {
 
 // GET / route
 router.get('/', checkUserSession, (req, res) => {
-    res.render('create', { user: req.session.user.username });
+    
+    // get users current language from database
+    // const userLanguage = req.session.user.language;
+    // const userLanguage = "Spanish";
+    const userLanguage = languages[0].language;
+    const userLanguageLevel = "A1";
+    const lessons = languages.find(lang => lang.language === userLanguage).lessons[userLanguageLevel];
+
+
+    res.render('create', { user: req.session.user, languages, lessons });
 });
+
 
 module.exports = router;
